515b15a3afb529cfc94a0f8303ea9d1a5d01ad3b
HrudayeshBY <156597106+HrudayeshBY@users.noreply.github.com>
2024-11-15 08:36:14 +0530

    Add files via upload

---
+++ ev_relay_latching.ino
@@ -0,0 +1,40 @@
+int relay1pin = 2;
+int relay2pin = 3;
+int relay3pin = 4;
+
+int button1Pin = 8;
+int button2Pin = 9;
+int button3Pin = 10;
+
+int button1Status = 0;
+int button2Status = 0;
+int button3Status = 0;
+
+void setup() {
+  pinMode(relay1pin, OUTPUT);
+  pinMode(relay2pin, OUTPUT);
+  pinMode(relay3pin, OUTPUT);
+
+  pinMode(button1Pin, INPUT);
+  pinMode(button2Pin, INPUT);
+  pinMode(button3Pin, INPUT);
+
+
+//setting all relays off
+  digitalWrite(relay1pin, HIGH);
+  digitalWrite(relay2pin, HIGH);
+  digitalWrite(relay3pin, HIGH);
+
+}
+
+void loop() {
+  
+  button1Status = digitalRead(button1Pin);
+  button2Status = digitalRead(button2Pin);
+  button3Status = digitalRead(button3Pin);
+
+  digitalWrite(relay1pin, !button1Status);
+  digitalWrite(relay2pin, !button2Status);
+  digitalWrite(relay3pin, !button3Status);
+
+}
